08511b7814d7a6f0a79c43d08c209b02b7743639
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-01-01 16:05:22 +0530

    Small Animation and Fixed Width Issue

--- components/auth/GoogleSignupButton.js
+++ components/auth/GoogleSignupButton.js
@@ -1,15 +1,36 @@
 import { signIn } from 'next-auth/react'
+import { useState } from 'react'
 import Image from 'next/image'
 
 const GoogleSignupButton = () => {
+  const [loading, setLoading] = useState(false)
+
+  const handleGoogleSignIn = async () => {
+    setLoading(true) // Start loading animation
+    try {
+      await signIn('google')
+    } catch (error) {
+      console.error('Sign-in failed:', error)
+    } finally {
+      setLoading(false) // Stop loading animation once the response comes
+    }
+  }
+
   return (
     <button
       type="button"
       className="bg-[#242C2F] w-[95%] xl:w-[70%] mx-auto rounded-[8px] py-2 mt-3 xl:my-3 gap-1 flex items-center justify-center inter-medium text-[20px] hover:bg-[#2E373B] hover:shadow-lg transition-all duration-200"
-      onClick={() => signIn('google')}
+      onClick={handleGoogleSignIn}
+      disabled={loading} // Disable the button while loading
     >
-      <Image src="/GoogleLogo.png" height={38} width={38} alt="Google Logo" />
-      <span className="capitalize">Continue With Google</span>
+      {loading ? (
+        <div className="animate-spin border-4 border-t-4 border-[#ffffff] border-solid w-6 h-6 rounded-full"></div>
+      ) : (
+        <Image src="/GoogleLogo.png" height={38} width={38} alt="Google Logo" />
+      )}
+      <span className="capitalize">
+        {loading ? 'Signing In...' : 'Continue With Google'}
+      </span>
     </button>
   )
 }
--- components/auth/SignoutButton.js
+++ components/auth/SignoutButton.js
@@ -5,7 +5,7 @@ import { signOut } from 'next-auth/react'
 const SignoutButton = () => {
   return (
     <button
-      className="py-2 w-[40%] rounded-[10px] bg-gradient-to-t to-[#060606] from-[#666666] text-white"
+      className="py-2 w-[40%] xl:w-[10%] xl:py-3 rounded-[10px] bg-gradient-to-t to-[#060606] from-[#666666] text-white inter-medium"
       onClick={() => {
         signOut()
       }}
