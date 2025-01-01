3f04834ebebbeeb68f6d0d39f4213062799754e8
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-01-01 16:02:45 +0530

    Lil Change

--- components/auth/HelpText.js
+++ components/auth/HelpText.js
@@ -4,9 +4,9 @@ import React from 'react'
 const HelpText = ({ position, margin, text, link }) => {
   return (
     <div
-      className={`w-[90%] mx-auto text-${position} ${margin} poppins-light text-[16px] text-black`}
+      className={`w-[90%] xl:w-[70%] mx-auto text-${position} ${margin}  poppins-light text-[16px] text-black `}
     >
-      <Link href={link} className="underline text-center poppins-extralight">
+      <Link href={link} className={`underline poppins-extralight`}>
         {text}
       </Link>
     </div>
